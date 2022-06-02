5b93a7a5b2889af151612daaf3cf55e37c4404e7
Paul Geser <paul.geser@roche.com>
2022-06-02 11:50:04 +0200

    modified gene files and added new endpoint

--- app/namespaces/wordspelling_check_namespaces.py
+++ app/namespaces/wordspelling_check_namespaces.py
@@ -3,11 +3,22 @@ from schemas.spellcheck_schema import SpellCheckModel, SpellCheckRequestModel
 from fastapi.routing import APIRouter
 from fastapi import status
 from spellchecker import SpellChecker
+from os import listdir
+from os.path import isfile, join
 
 wordspelling_check_namespace = APIRouter(prefix='/spelling')
 
 spell = SpellChecker(language=None, case_sensitive=True)
 spell.word_frequency.load_text_file('namespaces/dictionary.txt')
+
+files = [f for f in listdir('dictionary') if isfile(join('dictionary', f))]
+
+spell_checker = {}
+
+for file in files:
+    spell = SpellChecker(language=None, case_sensitive=True)
+    spell.word_frequency.load_text_file(f'dictionary/{file}')
+    spell_checker[file.split('.')[0]] = spell
 
 @wordspelling_check_namespace.post(
     '/suggestions',
@@ -39,3 +50,13 @@ def get_spelling_endpoint(word: str, type: str = 'gene'):
         "valid": bool(spell[word]),
         "initial_word": word
     }
+
+
+@wordspelling_check_namespace.get(
+    '/get-gene-types',
+    description='Get a list of all possible gene types',
+    status_code=status.HTTP_200_OK,
+)
+def get_all_gene_types():
+    file_names = [file.split('.')[0] for file in files]
+    return file_names
